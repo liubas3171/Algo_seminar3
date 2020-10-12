d61bbc46a691dc52c1a0c660e98a84304ac8b046
liubas3171 <54552855+liubas3171@users.noreply.github.com>
2020-10-12 08:43:02 +0300

    Update task_4.py

--- task_4.py
+++ task_4.py
@@ -1,43 +1,19 @@
-def partition(array, p, r):
-    '''
-    :param array: list of values
-    :param p: int, index of last element in the array
-    :param r: int, index of first element in the array
-    :return: int,
-    '''
-    x = array[r]
-    i = p - 1
-    for j in range(p, r):
-        if array[j] <= x:
-            i += 1
-            array[i], array[j] = array[j], array[i]
-    array[i + 1], array[r] = array[r], array[i + 1]
-    return i + 1
+def quick_sort(lst):
+    indexes = [(0, len(lst) - 1)]
+    while indexes:
+        start, end = indexes.pop()
+        x = lst[end]
+        i = start - 1
+        for j in range(start, end):
+            if lst[j] <= x:
+                i += 1
+                lst[j], lst[i] = lst[i], lst[j]
+        lst[i + 1], lst[end] = lst[end], lst[i + 1]
+        if start < i:
+            indexes.append((start, i))
+        if end > i + 2:
+            indexes.append((i + 2, end))
+    return lst
 
 
-def quick_sort(array, low, high):
-    '''
-    :param array: list of numbers
-    :param low: int, index of the very first element in array
-    :param high: int, index of last element in array
-    :return: None, sorts a given array.
-    '''
-    stack = []
-    stack.append(low)
-    stack.append(high)
-    while stack:
-        high = stack.pop()
-        low = stack.pop()
-        ind = partition(array, low, high)
-        if ind - 1 > low:
-            stack.append(low)
-            stack.append(ind - 1)
-        if ind + 1 < high:
-            stack.append(ind + 1)
-            stack.append(high)
-
-
-if __name__ == '__main__':
-    arr = [4, 3, 5, 2, 1, 3, 2, 3]
-    quick_sort(arr, 0, len(arr) - 1)
-    print(arr)
+print(quick_sort([44, 2, 7, 1, 9, 0, 5]))
